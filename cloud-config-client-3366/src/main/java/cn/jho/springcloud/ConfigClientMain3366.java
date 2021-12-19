23f9af113b31a4e743db24dfbc167109ce5232a1
xujihong <xu-jihong@qq.com>
2021-12-19 15:46:04 +0800

    feat: 集成消息总线实现全局广播动态刷新配置

---
+++ cloud-config-client-3366/src/main/java/cn/jho/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package cn.jho.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author JHO xu-jihong@qq.com
+ * @date 2021-12-19 14:08
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+
+
+}
