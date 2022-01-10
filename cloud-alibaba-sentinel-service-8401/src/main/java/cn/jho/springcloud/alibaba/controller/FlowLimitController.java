8ccf1426fc9cba08d6bb4e5d4e947306e89287e4
jho-yf <xu-jihong@qq.com>
2022-01-10 20:46:49 +0800

    feat: sentinel限流服务实例兜底处理

--- cloud-alibaba-sentinel-service-8401/src/main/java/cn/jho/springcloud/alibaba/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service-8401/src/main/java/cn/jho/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,11 @@
 package cn.jho.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/sentinel/flow-limit")
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/test1")
@@ -20,6 +25,24 @@ public class FlowLimitController {
     @GetMapping("/test2")
     public String test2() {
         return "------------test2";
+    }
+
+    /**
+     * 热点key测试
+     */
+    @GetMapping("/hot")
+    @SentinelResource(value = "hot", blockHandler = "dealHostKey")
+    public String hotKey(@RequestParam(value = "name", required = false) String name,
+                         @RequestParam(value = "age", required = false) Integer age) {
+        log.info("name={},age={}", name, age);
+        return "【hotKey】name=" + name + ",age=" + age;
+    }
+
+    /**
+     * 兜底处理
+     */
+    public String dealHostKey(String name, Integer age, BlockException exception) {
+        return "【blockHandler】name=" + name + ",age=" + age + ",exception=" + exception;
     }
 
 }
