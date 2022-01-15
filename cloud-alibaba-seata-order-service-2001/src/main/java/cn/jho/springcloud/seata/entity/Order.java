5327041ebdf47870caa587bc911c51126ddadf92
jho-yf <xu-jihong@qq.com>
2022-01-15 23:23:31 +0800

    feat: seata订单模块示例

---
+++ cloud-alibaba-seata-order-service-2001/src/main/java/cn/jho/springcloud/seata/entity/Order.java
@@ -0,0 +1,35 @@
+package cn.jho.springcloud.seata.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * @author JHO xu-jihong@qq.com
+ * @date 2022-01-15 17:48
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    private Long id;
+
+    private Long userId;
+
+    private Long productId;
+
+    private Integer count;
+
+    private BigDecimal money;
+
+    /**
+     * 订单状态：
+     *  0 创建中
+     *  1 已完结
+     */
+    private Integer status;
+
+}
