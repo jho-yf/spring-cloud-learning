1a69d406572b54f5e70704c9401c2ecba9a8d66b
jho-yf <xu-jihong@qq.com>
2022-01-23 10:18:30 +0800

    fix: 指定Order数据库表名为t_order

--- cloud-alibaba-seata-order-service-2001/src/main/java/cn/jho/springcloud/seata/entity/Order.java
+++ cloud-alibaba-seata-order-service-2001/src/main/java/cn/jho/springcloud/seata/entity/Order.java
@@ -1,5 +1,8 @@
 package cn.jho.springcloud.seata.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +16,10 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@TableName("t_order")
 public class Order {
 
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     private Long userId;
